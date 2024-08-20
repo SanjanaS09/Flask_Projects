b942f7804f334862a703bb114c429eeefcfc99f1
Sanjana <shettysanjana09@gmail.com>
2024-08-20 15:49:34 +0530

    extractcsv

---
+++ extractcsv/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, render_template, send_file, Response
+import pandas as pd
+import io
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/data', methods=['GET', 'POST'])
+def data():
+    if request.method == "POST":
+        uploaded_file = request.files.get('csvfile')
+        if not uploaded_file or not uploaded_file.filename.endswith('.csv'):
+            return 'Invalid file type'
+
+        df = pd.read_csv(uploaded_file)
+
+        selected_columns = ['First Name', 'Last Name', 'Email']
+        if not set(selected_columns).issubset(df.columns):
+            return "Some columns are not found in the uploaded file"
+
+        return render_template('data.html', columns=df.columns.tolist(), filename='uploaded_file')
+
+    return render_template('data.html')
+
+@app.route('/download', methods=['POST'])
+def download():
+    selected_columns = request.form.getlist('columns')
+
+    df = pd.read_csv(io.StringIO(request.form['csv_data']))
+
+    filtered_df = df[selected_columns]
+
+    output = io.StringIO()
+    filtered_df.to_csv(output, index=False)
+    output.seek(0)
+
+    return Response(output, mimetype="text/csv")
+
+if __name__ == '__main__':
+    app.run(debug=True)
